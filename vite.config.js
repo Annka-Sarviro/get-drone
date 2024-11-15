a4ab1ac8abe05bc969e4c580faeba8121412b1e9
Annka-Sarviro <annka_nuha@i.ua>
2024-11-15 11:49:21 +0200

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   root: "./src",
-  base: "/GetDrone/",
+  base: "/get-drone/",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
